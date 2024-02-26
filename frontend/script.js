08ccb1bd86c6ddbaf0ccadf6185bd98c886be7e3
Abel Lee <binghengabellee@gmail.com>
2024-02-26 21:15:20 +0800

    change to localhost

--- frontend/script.js
+++ frontend/script.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", async event => {
   const password = passwordInput.value
   const username = usernameInput.value
 
-  const result = await fetch('http://ec2-3-27-134-44.ap-southeast-2.compute.amazonaws.com/users', {
+  const result = await fetch('http://localhost:3000/users', {
     method: 'POST',
     mode: 'cors',
     headers: {
